096a5e63e844c2d87dfc118ea61f1118489cc427
hadar-c <cohen.hdr@gmail.com>
2021-10-07 18:34:06 +0300

    init-commit

---
+++ implementations/array.py
@@ -0,0 +1,6 @@
+# This function reverses a string
+def reverse(x):
+    # [From:To:Step]
+    return x[::-1]
+
+# class Array:
---
+++ main.py
@@ -0,0 +1,10 @@
+import implementations.array as array
+
+print('\033[1m' + '\033[94m' + "DATA STRUCTURES:" + '\033[0m')
+
+print('\033[1m' + '\033[92m' + "ARRAY:" + '\033[0m')
+
+str_to_rev = input("Enter a string to reverse:")
+print("Reversed string: ", array.reverse(str_to_rev))
+
+
